d63ea687db3896762140b2aa30634dd04ae863bb
Apoorv Choubey <theapoorvs1@gmail.com>
2017-10-10 00:51:05 +0530

    imGET add

--- works/imGET/js/main.js
+++ works/imGET/js/main.js
@@ -4,13 +4,13 @@ function search(){
     input = document.getElementById("search-box");
     filterString = input.value.toLowerCase();
 
-    for(var i=0; i<2; i++){
+    for(var i = 0; i < 4; i++){
 
         ulist = document.getElementsByTagName("ul")[i];
         litem = ulist.getElementsByTagName("li");
         for (var j = 0; j < litem.length; j++) {
             img = litem[j].getElementsByTagName("img")[0];
-            if (img.alt.toLowerCase().indexOf(filterString) > -1) {
+            if (img.alt.indexOf(filterString) > -1) {
                 litem[j].style.display = "";
             }
             else {
@@ -20,3 +20,9 @@ function search(){
     }
 
 }
+
+$(document).ready(function main(){
+    $(".hline").addClass("animated fadeInLeft");
+    $(".search > h1").addClass("animated bounce");
+    $(".gallery > ul > li").addClass("animated fadeIn");
+});
